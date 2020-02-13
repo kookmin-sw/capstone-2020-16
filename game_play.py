d71a5b5551d74f60c6878c50cea58fe2fa54c0e6
gkswjdzz <gkswjdzz@naver.com>
2020-02-13 17:31:24 +0900

    02/13

--- game_play.py
+++ game_play.py
@@ -4,20 +4,23 @@ import json
 
 from gamemanager import GameManager
 from utils.code_query import select_code
-
+from userprogram import UserProgram
 
 try:
-    game = os.path.join(os.getcwd(), 'game')
-    os.mkdir(game)
+    match = os.path.join(os.getcwd(), 'match')
+    problem_data = os.path.join(os.getcwd(), 'problem_data')
+    os.mkdir(match)
+    os.mkdir(problem_data)
+
 except Exception as e:
-    print(e)
+    print('error in make game folder :', e)
 
 extension = ['', '.c', '.cpp', '.py', '.java']
 
 def match_game(challenger_idx, oppositer_idx, problem_idx):
     temp = '{0}{1}{2}'.format(challenger_idx, oppositer_idx, problem_idx)
-    game_dir = os.path.join(game, temp)
-    os.mkdir(game_dir)
+    match_dir = os.path.join(match, temp)
+    os.mkdir(match_dir)
 
     challenger_code_data = select_code(challenger_idx, problem_idx)
     oppositer_code_data = select_code(oppositer_idx, problem_idx)
@@ -25,8 +28,8 @@ def match_game(challenger_idx, oppositer_idx, problem_idx):
     challenger_code_filename = '{0}{1}'.format(challenger_idx, extension[challenger_code_data.language_index])
     oppositer_code_filename = '{0}{1}'.format(oppositer_idx, extension[oppositer_code_data.language_index])
 
-    challenger_code_path = os.path.join(game_dir, challenger_code_filename)
-    oppositer_code_path = os.path.join(game_dir, oppositer_code_filename)
+    challenger_code_path = os.path.join(match_dir, challenger_code_filename)
+    oppositer_code_path = os.path.join(match_dir, oppositer_code_filename)
 
     with open(challenger_code_path, 'w') as f:
         f.write(challenger_code_data.code)
@@ -34,4 +37,13 @@ def match_game(challenger_idx, oppositer_idx, problem_idx):
     with open(oppositer_code_path, 'w') as f:
         f.write(oppositer_code_data.code)
 
-       
+    with open(os.path.join(problem_data, '{0}.json'.format(problem_idx))) as f:
+        data = json.load(f)
+
+    challenger = UserProgram(challenger_code_data.language, match_dir, challenger_code_filename)
+    oppositer = UserProgram(oppositer_code_data.language, match_dir, oppositer_code_filename)
+
+    game_manager = GameManager(challenger=challenger, oppositer=oppositer, data['placement'])
+
+
+
---
+++ userprogram.py
@@ -0,0 +1,10 @@
+class UserProgram():
+    def __init__(self, language, save_path, filename):
+        self.language = language
+        self.save_path = save_path
+        self.filename = filename
+
+    def compile(self):
+        pass
+
+    def
