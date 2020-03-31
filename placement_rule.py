40a5f2dc588c841f109094783456892fe74355fb
CMS <tyms0503@gmail.com>
2020-03-31 18:01:39 +0900

    change for api

--- placement_rule.py
+++ placement_rule.py
@@ -69,6 +69,7 @@ class PlacementRule:
             else:
                 print('else', placement)
                 self.obj_number = list(map(str, placement.split()))[0]
+                print('asd')
                 self.x = list(map(int, placement.split()))[1]
                 self.y = list(map(int, placement.split()))[2]
                 if self.check_range(self.x, self.y):
--- tasks.py
+++ tasks.py
@@ -29,27 +29,30 @@ def play_game(data):
     # run_container()
 
     match_data = data
+    print(match_data)
     match_dir = os.getcwd()  # os.path.join(os.getcwd(), 'match')
     extension = {'': '', 'C': '.c', 'C++': '.cpp', 'PYTHON': '.py', 'JAVA': '.java'}
 
     challenger_code_filename = 'challenger{0}'.format(extension[match_data['challenger_language']])
-    oppositer_code_filename = 'oppositer{0}'.format(extension[match_data['oppositer_language']])
+    oppositer_code_filename = 'oppositer{0}'.format(extension[match_data['opposite_language']])
 
     challenger_code_path = os.path.join(match_dir, challenger_code_filename)
     oppositer_code_path = os.path.join(match_dir, oppositer_code_filename)
 
-    challenger_code = select_code(match_data['challenger'], match_data['problem'])
-    oppositer_code = select_code(match_data['oppositer'], match_data['problem'])
+    #challenger_code = select_code(match_data['challenger'], match_data['problem'])
+    #oppositer_code = select_code(match_data['oppositer'], match_data['problem'])
+    challenger_code = match_data['challenger_code']
+    oppositer_code = match_data['opposite_code']
 
-    # with open(challenger_code_path, 'w') as f:
-    #     f.write(challenger_code)
-    #
-    # with open(oppositer_code_path, 'w') as f:
-    #     f.write(oppositer_code)
+    with open(challenger_code_path, 'w') as f:
+        f.write(challenger_code)
+    
+    with open(oppositer_code_path, 'w') as f:
+        f.write(oppositer_code)
 
     challenger = UserProgram(match_data['challenger'], match_data['challenger_language'], match_dir,
                              challenger_code_filename)
-    oppositer = UserProgram(match_data['oppositer'], match_data['oppositer_language'], match_dir,
+    oppositer = UserProgram(match_data['opposite'], match_data['opposite_language'], match_dir,
                             oppositer_code_filename)
 
     game_manager = GameManager(challenger=challenger, oppositer=oppositer,
@@ -59,15 +62,15 @@ def play_game(data):
                                obj_num=match_data['obj_num'])
 
     match_result, board_record, placement_record = game_manager.play_game()
-    with open('result.txt', 'w') as f:
-        f.write(match_result)
-    with open('result.txt', 'a') as f:
-        f.write(board_record)
-    with open('result.txt', 'a') as f:
-        f.write(placement_record)
+    #with open('result.txt', 'w') as f:
+    #    f.write(match_result)
+    #with open('result.txt', 'a') as f:
+    #    f.write(board_record)
+    #with open('result.txt', 'a') as f:
+    #    f.write(placement_record)
 
     #   update match data
-    update_match_data(match_result, board_record, placement_record)
+    #update_match_data(match_result, board_record, placement_record)
 
 
 # matchInfo = {
