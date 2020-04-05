93bad844bcc0c2e5af9c3e51f9f5aadc092d2761
CMS <tyms0503@gmail.com>
2020-04-06 01:53:41 +0900

    add Dockerfile

--- core/match_game.py
+++ core/match_game.py
@@ -10,21 +10,20 @@ from utils.util_match import update_match_data
 from userprogram import UserProgram
 
 
-def match(match_data):
-    match_dir = os.getcwd()     # os.path.join(os.getcwd(), 'match')
+def match(data):
+    match_data = data
+    match_dir = os.getcwd()  # os.path.join(os.getcwd(), 'match')
     extension = {'': '', 'C': '.c', 'C++': '.cpp', 'PYTHON': '.py', 'JAVA': '.java'}
-    # update_url = 'http://127.0.0.1:8000/api/v1/game/' + match_data['match_id']
+    update_url = 'http://203.246.112.32:8000/api/v1/game/' + str(match_data['match_id']) + '/'
 
     challenger_code_filename = 'challenger{0}'.format(extension[match_data['challenger_language']])
-    oppositer_code_filename = 'oppositer{0}'.format(extension[match_data['oppositer_language']])
+    oppositer_code_filename = 'oppositer{0}'.format(extension[match_data['opposite_language']])
 
     challenger_code_path = os.path.join(match_dir, challenger_code_filename)
     oppositer_code_path = os.path.join(match_dir, oppositer_code_filename)
 
-    # challenger_code = select_code(match_data['challenger'], match_data['problem'])
-    # oppositer_code = select_code(match_data['oppositer'], match_data['problem'])
     challenger_code = match_data['challenger_code']
-    oppositer_code = match_data['oppositer_code']
+    oppositer_code = match_data['opposite_code']
 
     with open(challenger_code_path, 'w') as f:
         f.write(challenger_code)
@@ -32,32 +31,36 @@ def match(match_data):
     with open(oppositer_code_path, 'w') as f:
         f.write(oppositer_code)
 
-    challenger = UserProgram(match_data['challenger'], match_data['challenger_language'], match_dir, challenger_code_filename)
-    oppositer = UserProgram(match_data['oppositer'], match_data['oppositer_language'], match_dir, oppositer_code_filename)
+    challenger = UserProgram(match_data['challenger'], match_data['challenger_language'], match_dir,
+                             challenger_code_filename)
+    oppositer = UserProgram(match_data['opposite'], match_data['opposite_language'], match_dir,
+                            oppositer_code_filename)
 
     game_manager = GameManager(challenger=challenger, oppositer=oppositer,
                                placement_rule=match_data['placement'], action_rule=match_data['action'],
-                               ending_rule=match_data['ending'], turn=match_data['turn'],
+                               ending_rule=match_data['ending'],
                                board_size=match_data['board_size'], board_info=match_data['board_info'],
                                obj_num=match_data['obj_num'])
 
-    match_result, board_record, placement_record = game_manager.play_game()
-    req = requests.put(update_url, data={'match_result':match_result, 'board_record':board_record, 'placement_record':placement_record})
+    winner, board_record, placement_record, result, error_msg = game_manager.play_game()
     # with open('result.txt', 'w') as f:
-    #     f.write(match_result)
+    #    f.write(match_result)
     # with open('result.txt', 'a') as f:
-    #     f.write(board_record)
+    #    f.write(board_record)
     # with open('result.txt', 'a') as f:
-    #     f.write(placement_record)
+    #    f.write(placement_record)
 
-    #   update match data
-    update_match_data(match_result, board_record, placement_record)
+    data = {"winner": winner, "record": board_record, "placement_record": placement_record, "result": result,
+            "error_msg": error_msg}
+    print(data)
+    r = requests.patch(update_url, data=data)
+    print('request ok')
 
 
 if __name__ == '__main__':
     # json_data = json.loads(sys.argv[1])
 
-    with open('dummy_data2.json') as json_file:
+    with open('match.json') as json_file:
         json_data = json.load(json_file)
     match(json_data)
 
--- core/tasks.py
+++ core/tasks.py
@@ -24,61 +24,59 @@ cpu_num = multiprocessing.cpu_count()
 docker_img = "app"
 
 
-# def play_game(json_data):  # match_data is json format
-@app.task
-def play_game(data):
-    # run_container()
-
-    match_data = data
-    print(match_data)
-    match_dir = os.getcwd()  # os.path.join(os.getcwd(), 'match')
-    extension = {'': '', 'C': '.c', 'C++': '.cpp', 'PYTHON': '.py', 'JAVA': '.java'}
-    print(match_data['match_id'])
-    update_url = 'http://203.246.112.32:8000/api/v1/game/' + str(match_data['match_id']) + '/'
-
-
-    challenger_code_filename = 'challenger{0}'.format(extension[match_data['challenger_language']])
-    oppositer_code_filename = 'oppositer{0}'.format(extension[match_data['opposite_language']])
-
-    challenger_code_path = os.path.join(match_dir, challenger_code_filename)
-    oppositer_code_path = os.path.join(match_dir, oppositer_code_filename)
-
-    #challenger_code = select_code(match_data['challenger'], match_data['problem'])
-    #oppositer_code = select_code(match_data['oppositer'], match_data['problem'])
-    challenger_code = match_data['challenger_code']
-    oppositer_code = match_data['opposite_code']
-
-    with open(challenger_code_path, 'w') as f:
-        f.write(challenger_code)
-    
-    with open(oppositer_code_path, 'w') as f:
-        f.write(oppositer_code)
-
-    challenger = UserProgram(match_data['challenger'], match_data['challenger_language'], match_dir,
-                             challenger_code_filename)
-    oppositer = UserProgram(match_data['opposite'], match_data['opposite_language'], match_dir,
-                            oppositer_code_filename)
-
-    game_manager = GameManager(challenger=challenger, oppositer=oppositer,
-                               placement_rule=match_data['placement'], action_rule=match_data['action'],
-                               ending_rule=match_data['ending'],
-                               board_size=match_data['board_size'], board_info=match_data['board_info'],
-                               obj_num=match_data['obj_num'])
-
-    winner, board_record, placement_record, result, error_msg = game_manager.play_game()
-    #with open('result.txt', 'w') as f:
-    #    f.write(match_result)
-    #with open('result.txt', 'a') as f:
-    #    f.write(board_record)
-    #with open('result.txt', 'a') as f:
-    #    f.write(placement_record)
-
-    #   update match data
-    #update_match_data(match_result, board_record, placement_record)
-    data = {"winner": winner, "record": board_record, "placement_record": placement_record, "result": result, "error_msg": error_msg}
-    print(data)
-    r = requests.patch(update_url, data=data)
-    print('request ok')
+# @app.task
+# def play_game(data):
+#
+    # match_data = data
+    # print(match_data)
+    # match_dir = os.getcwd()  # os.path.join(os.getcwd(), 'match')
+    # extension = {'': '', 'C': '.c', 'C++': '.cpp', 'PYTHON': '.py', 'JAVA': '.java'}
+    # print(match_data['match_id'])
+    # update_url = 'http://203.246.112.32:8000/api/v1/game/' + str(match_data['match_id']) + '/'
+    #
+    #
+    # challenger_code_filename = 'challenger{0}'.format(extension[match_data['challenger_language']])
+    # oppositer_code_filename = 'oppositer{0}'.format(extension[match_data['opposite_language']])
+    #
+    # challenger_code_path = os.path.join(match_dir, challenger_code_filename)
+    # oppositer_code_path = os.path.join(match_dir, oppositer_code_filename)
+    #
+    # #challenger_code = select_code(match_data['challenger'], match_data['problem'])
+    # #oppositer_code = select_code(match_data['oppositer'], match_data['problem'])
+    # challenger_code = match_data['challenger_code']
+    # oppositer_code = match_data['opposite_code']
+    #
+    # with open(challenger_code_path, 'w') as f:
+    #     f.write(challenger_code)
+    #
+    # with open(oppositer_code_path, 'w') as f:
+    #     f.write(oppositer_code)
+    #
+    # challenger = UserProgram(match_data['challenger'], match_data['challenger_language'], match_dir,
+    #                          challenger_code_filename)
+    # oppositer = UserProgram(match_data['opposite'], match_data['opposite_language'], match_dir,
+    #                         oppositer_code_filename)
+    #
+    # game_manager = GameManager(challenger=challenger, oppositer=oppositer,
+    #                            placement_rule=match_data['placement'], action_rule=match_data['action'],
+    #                            ending_rule=match_data['ending'],
+    #                            board_size=match_data['board_size'], board_info=match_data['board_info'],
+    #                            obj_num=match_data['obj_num'])
+    #
+    # winner, board_record, placement_record, result, error_msg = game_manager.play_game()
+    # #with open('result.txt', 'w') as f:
+    # #    f.write(match_result)
+    # #with open('result.txt', 'a') as f:
+    # #    f.write(board_record)
+    # #with open('result.txt', 'a') as f:
+    # #    f.write(placement_record)
+    #
+    # #   update match data
+    # #update_match_data(match_result, board_record, placement_record)
+    # data = {"winner": winner, "record": board_record, "placement_record": placement_record, "result": result, "error_msg": error_msg}
+    # print(data)
+    # r = requests.patch(update_url, data=data)
+    # print('request ok')
 
 # matchInfo = {
 #     "challenger": challenger.user.pk,
@@ -99,14 +97,20 @@ def play_game(data):
 # }
 
 
-# def run_container():
-#     print('run container')
-#     client = docker.from_env()
-#     containers_num = len(client.containers.list())
-#
-#     while containers_num >= cpu_num:
-#         time.sleep(1)
-#         print('not enough cpu_num. waiting.....')
-#
-#     client.containers.run(image=docker_img, command='echo hello world', auto_remove=False, tty=True, stdin_open=True)
+@app.task
+def run_container(data):
+    print('run container')
+    client = docker.from_env()
+    containers_num = len(client.containers.list())
+
+    while containers_num >= cpu_num:
+        time.sleep(1)
+        print('not enough cpu_num. waiting.....')
+
+    f_dir = os.getcwd()
+    match_data_file_path = os.path.join(f_dir, 'matchdata.json')
+    with open(match_data_file_path, 'w') as f:
+        f.write(data)
+
+    client.containers.run(image=docker_img, command='echo hello world', auto_remove=False, tty=True, stdin_open=True)
 
