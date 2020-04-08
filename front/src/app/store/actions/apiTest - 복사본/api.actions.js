26e22291ed44b460b959ca3dfb48f17a28c25228
parkhojun <mara8534@naver.com>
2020-04-09 01:23:27 +0900

    Update React-Redux Store

---
+++ front/src/app/store/actions/apiTest - 복사본/api.actions.js
@@ -0,0 +1,15 @@
+export const GET_USER = '[API] GET';
+// export const CLOSE_DIALOG = '[DIALOG] CLOSE';
+
+export function getUser() {
+	return {
+		type: GET_USER
+	};
+}
+
+// export function openDialog(options) {
+// 	return {
+// 		type: OPEN_DIALOG,
+// 		options
+// 	};
+// }
