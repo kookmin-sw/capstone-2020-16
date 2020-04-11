9714875f69c8613188d9d8b68c04a4364246b70d
CMS <tyms0503@gmail.com>
2020-04-11 16:06:22 +0900

    change check placement logic

--- core/placement_rule.py
+++ core/placement_rule.py
@@ -2,11 +2,12 @@
 import numpy as np
 
 
+# noinspection SpellCheckingInspection
 class PlacementRule:
     def __init__(self):
         self.placement_message = None
-        self.placement_rule_move_list = [self.cross, self.diagonal, self.eight_dir]
-        self.placement_rule_add_list = [self.add_close, self.add_any]
+        self.placement_rule_list = [self.cross, self.diagonal, self.eight_dir, self.add_close, self.add_any]
+        # self.placement_rule_addlist = [self.add_close, self.add_any]
         self.placement_rule_option = [self.block_move, self.take_out_and_add, self.only_reverse]
 
         self.placement_type = None
@@ -26,8 +27,8 @@ class PlacementRule:
         self.obj_rule = None
 
         self.obj_type = None
-        self.obj_move_method = None
-        self.obj_add_method = None
+        self.obj_placement_list = []
+        self.obj_add_method_list = []
         self.obj_option = None
 
     def check_placement_rule(self, data, board, placement):
@@ -35,28 +36,28 @@ class PlacementRule:
 
         # 룰에 맞는 함수들 리스트에 추가
         self.check_game_type()
-        self.rule_list.append(self.check_base_placement_rule)
-        self.add_rule_method()
-        self.add_rule_option()
-        for function in self.rule_list:
-            function()
-            if self.placement_message is not None:
-                return self.placement_message, self.board
+        if self.check_base_placement_rule() is False:
+            return self.placement_message, self.board
+        # self.add_rule_option()
+        for rule in self.rule_list:
+            self.placement_rule_list[rule[1]](rule)
+            if self.placement_message == 'OK':
+                break
 
-        if self.placement_type == 'move':
-            self.board[self.x1][self.y1] = 0
-            self.board[self.x][self.y] = self.obj_number
-        elif self.placement_type == 'add':
-            self.board[self.x][self.y] = self.obj_number
+        if self.placement_message == 'OK':
+            if self.placement_type == 'move':
+                self.board[self.x1][self.y1] = 0
+                self.board[self.x][self.y] = self.obj_number
+            elif self.placement_type == 'add':
+                self.board[self.x][self.y] = self.obj_number
+        else:
+            self.placement_message = f'miss position {self.x,self.y}'
 
-        self.placement_message = 'OK'
         return self.placement_message, self.board
 
     def setting(self, data, board, placement):
         self.data = data
-
         try:
-            print('asdasd', type(placement))
             if '>' in placement:
                 self.x1 = list(map(int, placement.split('>')[0].split()))[0]
                 self.y1 = list(map(int, placement.split('>')[0].split()))[1]
@@ -78,12 +79,10 @@ class PlacementRule:
             self.board = board
             self.placement = placement
             self.rule_list.clear()
-            self.obj_rule = data.placement_rule[self.obj_number]  # ["이동", ["방향","가로","세로"],"옵션"]
+            self.obj_rule = data.placement_rule[self.obj_number]   # [[["이동or추가", "몇번이동", "최소 or x","최대 or y"], [이동2, , ,], ["추가", "최소", "최대"]],"옵션"]
             self.obj_type = self.obj_rule[0]
-            if self.obj_rule[1][0]:
-                self.obj_move_method = self.obj_rule[1][0]
-            if self.obj_rule[1][1]:
-                self.obj_add_method = self.obj_rule[1][1]
+            for rule in self.obj_rule[1]:
+                self.rule_list.append(rule)
             if self.obj_rule[2]:
                 self.obj_option = self.obj_rule[2]
             self.placement_message = None
@@ -91,27 +90,6 @@ class PlacementRule:
             self.placement_message = f'error in parsing user placement: {e}'
             print(self.placement_message)
             raise Exception(self.placement_message)
-
-    # noinspection PyMethodMayBeStatic
-    def check_base_placement_rule(self):  # rule 0 : check if user's placement where the stone is
-        # 유저의 착수타입과 오브젝트의 착수 타입이 일치하는지 확인됨
-        if self.placement_type == 'move':
-            if (self.x < 0 or self.x > self.data.board_size) or (self.y < 0 or self.y > self.data.board_size):
-                self.placement_message = f'out of the board : {self.x, self.y}. {self.x1, self.y1} > {self.x, self.y}'
-            elif self.board[self.x1][self.y1] == 0:
-                self.placement_message = f'There is no stone : {self.x1, self.y1}. {self.x1, self.y1} > {self.x, self.y}'
-        elif self.placement_type == 'add':
-            if (self.x < 0 or self.x > self.data.board_size) or (self.y < 0 or self.y > self.data.board_size):
-                self.placement_message = f'out of the board : {self.x, self.y}'
-
-        if self.board[self.x][self.y] > 0:
-            self.placement_message = f'There is already a stone {self.x, self.y}'
-
-        if self.board[self.x][self.y] < 0:
-            if 1 in self.obj_option:
-                pass
-            else:
-                self.placement_message = f'There is already a enemy stone {self.x, self.y}'
 
     # 착수 종류
     def check_game_type(self):
@@ -126,11 +104,39 @@ class PlacementRule:
             else:
                 self.placement_message = f'object{self.obj_number}{self.x, self.y} is add rule'
 
-    def add_rule_method(self):
+    # noinspection PyMethodMayBeStatic
+    def check_base_placement_rule(self):  # rule 0 : check if user's placement where the stone is
+        # 유저의 착수타입과 오브젝트의 착수 타입이 일치하는지 확인됨
         if self.placement_type == 'move':
-            self.rule_list.append(self.placement_rule_move_list[self.obj_move_method[0]])
+            if (self.x < 0 or self.x > self.data.board_size) or (self.y < 0 or self.y > self.data.board_size):
+                self.placement_message = f'out of the board : {self.x, self.y}. {self.x1, self.y1} > {self.x, self.y}'
+                return False
+            elif self.board[self.x1][self.y1] == 0:
+                self.placement_message = f'There is no stone : {self.x1, self.y1}. {self.x1, self.y1} > {self.x, self.y}'
+                return False
         elif self.placement_type == 'add':
-            self.rule_list.append(self.placement_rule_add_list[self.obj_add_method[0]])
+            if (self.x < 0 or self.x > self.data.board_size) or (self.y < 0 or self.y > self.data.board_size):
+                self.placement_message = f'out of the board : {self.x, self.y}'
+                return False
+
+        if self.board[self.x][self.y] > 0:
+            self.placement_message = f'There is already a stone {self.x, self.y}'
+            return False
+
+        if self.board[self.x][self.y] < 0:
+            if 1 in self.obj_option:
+                return True
+            else:
+                self.placement_message = f'There is already a enemy stone {self.x, self.y}'
+                return False
+
+    def add_rule_method(self):
+        pass
+        # if self.placement_type == 'move':
+        #     self.rule_list.append(self.placement_rule_move_list[self.obj_placement_list[0]])
+        # elif self.placement_type == 'add':
+        #     self.rule_list.append(self.placement_rule_add_list[self.obj_add_method_list[0]])
+        # self.rule_list.append(self.placement_rule_list[])
 
     def add_rule_option(self):
         if self.obj_option is not None:
@@ -138,9 +144,11 @@ class PlacementRule:
                 self.rule_list.append(self.placement_rule_option[option])
 
     # 이동일 때
-    def cross(self):  # 4방 십자
-        min_distance = self.obj_move_method[1]
-        max_distance = self.obj_move_method[2]
+    def cross(self, rule):  # 4방 십자
+        if self.placement_type == 'add':
+            return
+        min_distance = rule[1]
+        max_distance = rule[2]
         x_inc = abs(self.x1 - self.x)
         y_inc = abs(self.y1 - self.y)
         if max_distance == 0:
@@ -150,16 +158,19 @@ class PlacementRule:
                 min_distance <= y_inc <= max_distance:
             return True
         else:
-            if (x_inc > max_distance or x_inc < min_distance) or (y_inc > max_distance or y_inc < min_distance):
-                self.placement_message = f'out of object range {self.x1, self.y1} > {self.x, self.y}. object number : {self.obj_number}'
-                return False
-            else:
-                self.placement_message = f'object{self.obj_number} is cross rule. {self.x1, self.y1} > {self.x, self.y}'
-                return False
+            # if (x_inc > max_distance or x_inc < min_distance) or (y_inc > max_distance or y_inc < min_distance):
+            #     self.placement_message = f'out of object range {self.x1, self.y1} > {self.x, self.y}. object number : {self.obj_number}'
+            #     return False
+            # else:
+            #     self.placement_message = f'object{self.obj_number} is cross rule. {self.x1, self.y1} > {self.x, self.y}'
+            #     return False
+            return False
 
-    def diagonal(self):  # 4방 대각선
-        min_distance = self.obj_move_method[1]
-        max_distance = self.obj_move_method[2]
+    def diagonal(self, rule):  # 4방 대각선
+        if self.placement_type == 'add':
+            return
+        min_distance = rule[1]
+        max_distance = rule[2]
         x_inc = abs(self.x1 - self.x)
         y_inc = abs(self.y1 - self.y)
         if max_distance == 0:
@@ -167,21 +178,19 @@ class PlacementRule:
         if x_inc == y_inc and \
                 min_distance <= x_inc <= max_distance and \
                 min_distance <= y_inc <= max_distance:
+            self.placement_message = 'OK'
             return True
         else:
-            if (x_inc > max_distance or x_inc < min_distance) or (y_inc > max_distance or y_inc < min_distance):
-                self.placement_message = f'out of object range {self.x1, self.y1} > {self.x, self.y}. object number : {self.obj_number}'
-                return False
-            else:
-                self.placement_message = f'object{self.obj_number} is diagonal rule. {self.x1, self.y1} > {self.x, self.y}'
-                return False
+            return False
 
-    def eight_dir(self):  # 8방
-        if self.diagonal() is True or self.cross() is True:
-            pass
+    def eight_dir(self, rule):  # 8방
+        if self.placement_type == 'add':
+            return
+        if self.diagonal(rule) is True or self.cross(rule) is True:
+            return True
         else:
-            min_distance = self.obj_move_method[1]
-            max_distance = self.obj_move_method[2]
+            min_distance = rule[1]
+            max_distance = rule[2]
             x_inc = abs(self.x1 - self.x)
             y_inc = abs(self.y1 - self.y)
             if max_distance == 0:
@@ -191,23 +200,27 @@ class PlacementRule:
             else:
                 self.placement_message = f'object{self.obj_number} is 8 direction rule. {self.x1, self.y1} > {self.x, self.y}'
 
-    def custom(self):
-        # 가로 y칸, 세로 x칸 TODO
-        if abs(self.y - self.y1) == self.obj_move_method[2] and abs(self.x - self.x1) == self.obj_move_method[1]:
+    def custom(self, rule):
+        # 가로 y칸, 세로 x칸
+        if self.placement_type == 'add':
+            return
+        if abs(self.y - self.y1) == rule[2] and abs(self.x - self.x1) == rule[1]:
             pass
         else:
-            self.placement_message = f'object{self.obj_number} is x:{self.obj_move_method[1]} y:{self.obj_move_method[2]} rule. {self.x1, self.y1} > {self.x, self.y}'
+            self.placement_message = f'object{self.obj_number} is x:{rule[1]} y:{rule[2]} rule. {self.x1, self.y1} > {self.x, self.y}'
 
     # 새로운돌 착수
-    def add_close(self):  # 추가시 거리 - 인접
+    def add_close(self, rule):  # 추가시 거리 - 인접
+        if self.placement_type == 'move':
+            return
         dirr = []
-        if self.obj_add_method[1] == 0:  # 4방
+        if self.obj_add_method_list[1] == 0:  # 4방
             dirr = [(0, 1), (1, 0), (0, -1), (-1, 0)]
             self.placement_message = f'object{self.obj_number} is add close(cross) rule. {self.x, self.y}'
-        elif self.obj_add_method[1] == 1:
+        elif self.obj_add_method_list[1] == 1:   # 대각선
             dirr = [(-1, 1), (1, 1), (1, -1), (-1, -1)]
             self.placement_message = f'object{self.obj_number} is add close(diagonal) rule. {self.x, self.y}'
-        elif self.obj_add_method[1] == 2:
+        elif self.obj_add_method_list[1] == 2:   # 8방
             dirr = [(0, 1), (1, 0), (0, -1), (-1, 0), (-1, 1), (1, 1), (1, -1), (-1, -1)]
             self.placement_message = f'object{self.obj_number} is add close(8 dir) rule. {self.x, self.y}'
 
