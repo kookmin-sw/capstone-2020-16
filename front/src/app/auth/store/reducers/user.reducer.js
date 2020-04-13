e3ac7431adbe29dd30a9f7c19059db7874db3427
Suryeon Kang <srkang0913@kookmin.ac.kr>
2020-04-13 20:02:43 +0900

    autologout success

--- front/src/app/auth/store/reducers/user.reducer.js
+++ front/src/app/auth/store/reducers/user.reducer.js
@@ -3,9 +3,9 @@ import * as Actions from '../actions';
 const initialState = {
 	role: [], // guest
 	data: {
-		displayName: 'John Doe',
-		photoURL: 'assets/images/avatars/Velazquez.jpg',
-		email: 'johndoe@withinpixels.com',
+		displayName: 'ㅇㅅㅇ',
+		photoURL: 'assets/images/avatars/algos_lab.jpg',
+		email: 'algos_lab@google.com',
 		shortcuts: ['calendar', 'mail', 'contacts', 'todo']
 	}
 };
--- front/src/app/main/pages/profile/ProfilePage.js
+++ front/src/app/main/pages/profile/ProfilePage.js
@@ -31,75 +31,88 @@ function ProfilePage() {
 	}
 
 	return (
-		<FusePageSimple
-			classes={{
-				header: classes.layoutHeader,
-				toolbar: 'px-16 sm:px-24'
-			}}
-			header={
-				<div className="p-24 flex flex-1 flex-col items-center justify-center md:flex-row md:items-end">
-					<div className="flex flex-1 flex-col items-center justify-center md:flex-row md:items-center md:justify-start">
-						<FuseAnimate animation="transition.expandIn" delay={300}>
-							<Avatar className="w-96 h-96" src="assets/images/avatars/Velazquez.jpg" />
-						</FuseAnimate>
-						<FuseAnimate animation="transition.slideLeftIn" delay={300}>
-							<Typography className="md:mx-24" variant="h4" color="inherit">
-								John Doe
-							</Typography>
-						</FuseAnimate>
-					</div>
+    <FusePageSimple
+      classes={{
+        header: classes.layoutHeader,
+        toolbar: "px-16 sm:px-24"
+      }}
+      header={
+        <div className="p-24 flex flex-1 flex-col items-center justify-center md:flex-row md:items-end">
+          <div className="flex flex-1 flex-col items-center justify-center md:flex-row md:items-center md:justify-start">
+            <FuseAnimate animation="transition.expandIn" delay={300}>
+              <Avatar
+                className="w-96 h-96"
+                src="assets/images/avatars/profile.jpg" // 여기도 profile사진 받아오기
+              />
+            </FuseAnimate>
+            <FuseAnimate animation="transition.slideLeftIn" delay={300}>
+              <Typography className="md:mx-24" variant="h4" color="inherit">
+                username 받아오기
+              </Typography>
+            </FuseAnimate>
+          </div>
 
-					<div className="flex items-center justify-end">
-						<Button className="mx-8 normal-case" variant="contained" color="secondary" aria-label="Follow">
-							Follow
-						</Button>
-						<Button className="normal-case" variant="contained" color="primary" aria-label="Send Message">
-							Send Message
-						</Button>
-					</div>
-				</div>
-			}
-			contentToolbar={
-				<Tabs
-					value={selectedTab}
-					onChange={handleTabChange}
-					indicatorColor="primary"
-					textColor="primary"
-					variant="scrollable"
-					scrollButtons="off"
-					classes={{
-						root: 'h-64 w-full border-b-1'
-					}}
-				>
-					<Tab
-						classes={{
-							root: 'h-64'
-						}}
-						label="Timeline"
-					/>
-					<Tab
-						classes={{
-							root: 'h-64'
-						}}
-						label="About"
-					/>
-					<Tab
-						classes={{
-							root: 'h-64'
-						}}
-						label="Photos & Videos"
-					/>
-				</Tabs>
-			}
-			content={
-				<div className="p-16 sm:p-24">
-					{selectedTab === 0 && <TimelineTab />}
-					{selectedTab === 1 && <AboutTab />}
-					{selectedTab === 2 && <PhotosVideosTab />}
-				</div>
-			}
-		/>
-	);
+          <div className="flex items-center justify-end">
+            <Button
+              className="mx-8 normal-case"
+              variant="contained"
+              color="secondary"
+              aria-label="Follow"
+            >
+              Follow
+            </Button>
+            <Button
+              className="normal-case"
+              variant="contained"
+              color="primary"
+              aria-label="Send Message"
+            >
+              Send Message
+            </Button>
+          </div>
+        </div>
+      }
+      contentToolbar={
+        <Tabs
+          value={selectedTab}
+          onChange={handleTabChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="scrollable"
+          scrollButtons="off"
+          classes={{
+            root: "h-64 w-full border-b-1"
+          }}
+        >
+          <Tab
+            classes={{
+              root: "h-64"
+            }}
+            label="Timeline"
+          />
+          <Tab
+            classes={{
+              root: "h-64"
+            }}
+            label="About"
+          />
+          <Tab
+            classes={{
+              root: "h-64"
+            }}
+            label="Photos & Videos"
+          />
+        </Tabs>
+      }
+      content={
+        <div className="p-16 sm:p-24">
+          {selectedTab === 0 && <TimelineTab />}
+          {selectedTab === 1 && <AboutTab />}
+          {selectedTab === 2 && <PhotosVideosTab />}
+        </div>
+      }
+    />
+  );
 }
 
 export default ProfilePage;
