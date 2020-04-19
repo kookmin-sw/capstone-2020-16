78785f4e4992e18d68c1ac0ea9e6127d2c25ed6d
CMS <tyms0503@gmail.com>
2020-04-20 02:04:24 +0900

    add timeout func

--- core/execute_code.py
+++ core/execute_code.py
@@ -1,9 +1,7 @@
 import os
 import time
-# import signal
-import ptrace
-# import resource
-# from errorCode import *
+import signal
+import timeout
 
 
 class Execution:
@@ -21,16 +19,16 @@ class Execution:
             if '<' in command:
                 redirection_stdin = os.open(os.path.join(path, 'board.txt'), os.O_RDONLY)
                 os.dup2(redirection_stdin, 0)
-            # ptrace.traceme()
             os.execv(command[0], tuple(command[1:]))
 
         else:
             print('else', pid)
-            result, time = self.trace_program(pid)
+            try:
+                result, time = self.trace_program(pid)
+            except Exception as e:
+                os.kill(pid, signal.SIGSTOP)
+                return 'time over'
 
-            if time > self.limit_time:
-                return f'time over'
-            print('ppid', pid)
             if '<' in command:
                 while not os.path.isfile("placement.txt"):
                     time.sleep(0.3)
@@ -40,23 +38,11 @@ class Execution:
                 return pos
             else:
                 pass
-
+    
+    @timeout.timeout(2)
     def trace_program(self, pid):
         while True:
-            print('asdasd')
             wpid, status, res = os.wait4(pid, 0)
             # normal termination
             if os.WIFEXITED(status):
                 return True, res[0]
-
-            # abnormal termination
-            elif os.WIFSIGNALED(status):
-                try:
-                    ptrace.kill(pid)
-                except Exception as e:
-                    pass
-
-                return RUNTIME_ERROR, res[0]
-
-            else:
-                ptrace.syscall(pid, 0)
