32d0315c4b1d29e5832bd294c3cb67f6c8d06790
parkhojun <mara8534@naver.com>
2020-05-27 15:48:53 +0900

    Update By Required

--- front/src/app/main/apps/game/addgame/components/Checkbox3-2.js
+++ front/src/app/main/apps/game/addgame/components/Checkbox3-2.js
@@ -117,6 +117,7 @@ export default function SimpleCard() {
                         return (
                           <FormControl component="fieldset">
                             <RadioGroup aria-label="select3-5" name="select3-5" value={value3} onChange={handleChange3}>
+                              <FormControlLabel value="없음" control={<Radio />} label="없음" onClick={()=>{sessionStorage.setItem("actionDirection3", "없음")}} />
                               <FormControlLabel value="양 옆" control={<Radio />} label="양 옆" onClick={()=>{sessionStorage.setItem("actionDirection3", "양 옆")}} />
                               <FormControlLabel value="위 아래" control={<Radio />} label="위 아래" onClick={()=>{sessionStorage.setItem("actionDirection3", "위 아래")}}/>
                               <FormControlLabel value="X 방향" control={<Radio />} label="X 방향" onClick={()=>{sessionStorage.setItem("actionDirection3", "X 방향")}}/>
