f36ba1ad9b705aa74fcb1522dc28747d9c9c8eb6
kkoma2623 <boo106@kookmin.ac.kr>
2020-06-01 20:49:08 +0900

    add select problem page

--- front/src/app/main/apps/game/selfBattle/SelfBattle.js
+++ front/src/app/main/apps/game/selfBattle/SelfBattle.js
@@ -1,75 +1,139 @@
-import React from 'react';
-import { IonPhaser } from '@ion-phaser/react'
-import Scene1 from './components/Scene1.js'
-import Scene2 from './components/Scene2.js'
-import Modal from '@material-ui/core/Modal';
+import FuseAnimate from '@fuse/core/FuseAnimate';
+import FuseAnimateGroup from '@fuse/core/FuseAnimateGroup';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import Divider from '@material-ui/core/Divider';
 import { makeStyles } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import clsx from 'clsx';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import * as Actions from 'app/store/actions';
+import CardMedia from '@material-ui/core/CardMedia';
+import axios from 'axios';
+import SelfBattleButton from './SelfBattleButton'
 
-
-
-function getModalStyle() {
-	const top = 50 ;
-	const left = 50 ;
-  
-	return {
-	  top: `${top}%`,
-	  left: `${left}%`,
-	  transform: `translate(-${top}%, -${left}%)`,
-	};
-}
 const useStyles = makeStyles(theme => ({
-	paper: {
-		position: 'absolute',
-		// width: 400,
-		backgroundColor: theme.palette.background.paper,
-		// border: '2px solid #000',
-		boxShadow: theme.shadows[5],
-		padding: theme.spacing(2, 4, 3),
+	header: {
+		background: `linear-gradient(to right, ${theme.palette.primary.dark} 0%, ${theme.palette.primary.main} 100%)`,
+		color: theme.palette.getContrastText(theme.palette.primary.main)
 	},
+	headerIcon: {
+		position: 'absolute',
+		top: -64,
+		left: 0,
+		opacity: 0.04,
+		fontSize: 512,
+		width: 512,
+		height: 512,
+		pointerEvents: 'none'
+	}
 }));
 
-function SelfBattle() {
-	window.localStorage.setItem('game_id', 1959);
-	const classes = useStyles();
-	const [modalStyle] = React.useState(getModalStyle);
-	const [open, setOpen] = React.useState(false);
-	
-	const handleOpen = () => {
-		window.localStorage.setItem('game_id', 1959);
-		setOpen(true);
-	};
-	
-	const handleClose = () => {
-		window.localStorage.removeItem('game_id')
-		setOpen(false);
-	};
 
-	const game = {
-		width: 1050,
-		height: 700,
-		backgroundColor: 0x192d3f,
-		scene: [Scene1, Scene2],
-		pixelArt: true,
-	}
+function Courses(props) {
+
+	const dispatch = useDispatch();
+	var header = {
+		'Authorization' : 'jwt ' + window.localStorage.getItem('jwt_access_token')
+	  }
+
+	const classes = useStyles(props);
+
+	const [posts, setPosts] = useState([]);
+	
+
+
+	useEffect(() => {
+
+		axios
+		.get('http://203.246.112.32:8000/api/v1/problem/')
+		.then(response => {
+			dispatch(Actions.getProblemId(response.data.results));
+			setPosts(response.data.results);
+			window.sessionStorage.setItem('SS_ProblemId', response.data.results);
+		})
+	},[dispatch]);
+
 	
 	return (
-		<div>
-			<IconButton onClick={handleOpen} component="span">
-				<h3>{"self battle"}</h3>
-			</IconButton>
-			<Modal
-				aria-labelledby="simple-modal-title"
-				aria-describedby="simple-modal-description"
-				open={open}
-				onClose={handleClose}
+		<div className="flex flex-col flex-auto flex-shrink-0 w-full">
+			
+			<div
+				className={clsx(
+					classes.header,
+					'relative overflow-hidden flex flex-col flex-shrink-0 items-center justify-center text-center p-16 sm:p-24 h-200 sm:h-288'
+				)}
 			>
-				<div style={modalStyle} className={classes.paper}>
-					<IonPhaser game={game}/>
-				</div>
-			</Modal>
+				<FuseAnimate animation="transition.slideUpIn" duration={400} delay={100}>
+					<Typography color="inherit" className="text-24 sm:text-40 font-light">
+					Self Battle
+					</Typography>
+				</FuseAnimate>
+				<FuseAnimate duration={400} delay={600}>
+					<Typography variant="subtitle1" color="inherit" className="mt-8 sm:mt-16 mx-auto max-w-512">
+						<span className="opacity-75">
+						Welcome to Self Battle Mode. Choose a Game to Play!
+						</span>
+					</Typography>
+				</FuseAnimate>
+		
+			</div>
+			<div className="flex flex-col flex-1 max-w-2xl w-full mx-auto px-8 sm:px-16 py-24">
+		
+							<FuseAnimateGroup
+								enter={{
+									animation: 'transition.slideUpBigIn'
+								}}
+								className="flex flex-wrap py-24"
+							>	
+								{posts.map(problem => {
+									return (
+										<div className="w-full pb-24 sm:w-1/2 lg:w-1/3 sm:p-16" key={problem.title}>
+											<Card elevation={1} className="flex flex-col h-256">
+												<div
+													className="flex flex-shrink-0 items-center justify-between px-24 h-64"
+												>
+													<Typography className="font-medium truncate" color="inherit">
+														{problem.title}
+													</Typography>
+													
+				
+												</div>
+												<CardMedia className="flex items-center justify-center">
+												<Link className="font-medium" 										
+												to={`/SelfBattle/${problem.id}`}>
+												<img src={`assets/images/games/${problem.id}.jpg`} 
+													onClick = {() =>{
+														window.sessionStorage.setItem('SS_SelectedProblemId', problem.id);
+													}}
+													width='300' alt='thumbnail'></img>
+												</Link>
+												</CardMedia>
+												
+
+												<Divider />
+												<CardActions className="justify-center" >
+												<Link className="font-medium" 										
+												to={`/SelfBattle/${problem.id}`}>
+													 <button onClick = {() => {
+														 window.sessionStorage.setItem('SS_SelectedProblemId', problem.id);
+													 }}> <SelfBattleButton></SelfBattleButton> </button>
+												</Link>
+												<Divider orientation="vertical" flexItem />
+												</CardActions>
+												
+											</Card>
+										</div>
+									);
+								})}
+							</FuseAnimateGroup>
+						  
+			</div>
 		</div>
 	);
 }
 
-export default SelfBattle;
+// export default withReducer('academyApp', reducer)(Courses);
+export default (Courses);
