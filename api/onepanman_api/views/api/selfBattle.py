5cb26ddbc4830b3f387a7597d475c7d737c12907
woomurf <dngusdnd@gmail.com>
2020-06-04 21:49:21 +0900

    selfBattle error log

--- api/onepanman_api/views/api/selfBattle.py
+++ api/onepanman_api/views/api/selfBattle.py
@@ -19,37 +19,42 @@ class SelfBattle(APIView):
         code = Code.objects.all().filter(id=request.data['code'])[0]
         problem = Problem.objects.all().filter(id=request.data['problem'])[0]
         rule = json.loads(problem.rule)
+        try:
+            matchInfo = {
+                "challenger": user.user.pk,
+                "opposite": user.user.pk,
+                "challenger_code_id": code.id,
+                "opposite_code_id": code.id,
+                "challenger_code": code.code,
+                "opposite_code": code.code,
+                "challenger_language": code.language.name,
+                "opposite_language": code.language.name,
+                "problem": problem.id,
+                "obj_num": rule["obj_num"],
+                "placement": rule["placement"],
+                "action": rule["action"],
+                "ending": rule["ending"],
+                "board_size": problem.board_size,
+                "board_info": request.data['board_info'],
+                "placement_info": request.data['placement_info']
+            }
+        except Exception as e :
+            print("matchInfo_Error : {}".format(e))
 
-        matchInfo = {
-            "challenger": user.user.pk,
-            "opposite": user.user.pk,
-            "challenger_code_id": code.id,
-            "opposite_code_id": code.id,
-            "challenger_code": code.code,
-            "opposite_code": code.code,
-            "challenger_language": code.language.name,
-            "opposite_language": code.language.name,
-            "problem": problem.id,
-            "obj_num": rule["obj_num"],
-            "placement": rule["placement"],
-            "action": rule["action"],
-            "ending": rule["ending"],
-            "board_size": problem.board_size,
-            "board_info": request.data['board_info'],
-            "placement_info": request.data['placement_info']
-        }
+        try:
+            # celery에 넘겨줌
+            result = tasks.play_with_me.delay(matchInfo)
 
-        # celery에 넘겨줌
-        result = tasks.play_with_me.delay(matchInfo)
+            # redis로 받음
+            r = redis.StrictRedis(host="192.168.23.13", port=6379, db=0)
+            dict_name = str(user.user.pk) + '_' + str(code.id)
+            print(dict_name)
 
-        # redis로 받음
-        r = redis.StrictRedis(host="192.168.23.13", port=6379, db=0)
-        dict_name = str(user.user.pk) + '_' + str(code.id)
-        print(dict_name)
-
-        while r.exists(dict_name) == 0:
-            pass
-        json_dict = r.get(dict_name).decode('utf-8')
-        test_dict = dict(json.loads(json_dict))
+            while r.exists(dict_name) == 0:
+                pass
+            json_dict = r.get(dict_name).decode('utf-8')
+            test_dict = dict(json.loads(json_dict))
+        except Exception as e:
+            print("redis&celery error : {}".format(e))
 
         return Response(test_dict, status=status.HTTP_200_OK)
