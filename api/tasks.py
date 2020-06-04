37c406b3f0a98628f8f7c8374ed3350d6af6d091
woomurf <dngusdnd@gmail.com>
2020-06-04 22:44:21 +0900

    api/tasks.py

--- api/tasks.py
+++ api/tasks.py
@@ -6,3 +6,11 @@ app = Celery('tasks', broker='redis://192.168.23.13:6379')
 @app.task
 def play_game(data):
     pass
+
+@app.task
+def play_with_me(data):
+    pass
+
+@app.tasks
+def test_code(data):
+    pass
